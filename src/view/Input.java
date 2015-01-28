a75535d7031fdda23e991c876c78c68fc767541b
mm280 <mm280@st-andrews.ac.uk>
2015-01-28 13:18:04 +0000

    Added public key input

--- src/view/Input.java
+++ src/view/Input.java
@@ -4,20 +4,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Input implements IView{
-	
-	public String getInput(String message) {
+public class Input implements IView {
+
+	public String getInput(String message, BufferedReader reader) {
 		System.out.println(message);
-		
+
 		String input = null;
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			input = reader.readLine();
-			reader.close();
 		} catch (IOException e) {
-			System.err.println("A problem occured reading input from the console.");
+			System.err.println("A problem occurred reading input from the console.");
 		}
 		return input;
 	}
-	
+
+	public int getPort(String message, BufferedReader reader) {
+		System.out.println(message);
+		int port = 0;
+		String input = null;
+		try{
+			input = reader.readLine();
+			try {
+				port = Integer.parseInt(input);
+				if (port <= 0) {
+					getPort("Please enter the port number: ", reader);
+				}
+			}catch(NumberFormatException e){
+				getPort("Please enter a number: ", reader);
+			}
+
+		} catch (IOException e) {
+			System.err.println("A problem occurred reading input from the console.");
+		}
+
+		return port;
+	}
 }
