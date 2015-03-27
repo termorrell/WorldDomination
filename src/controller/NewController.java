f44ba984e936d3532f9409e73f3288eb88d43236
Caroline <cih4@st-andrews.ac.uk>
2015-03-27 19:25:54 +0000

    fix any errors and start on new controller

--- src/controller/NewController.java
+++ src/controller/NewController.java
@@ -1,35 +1,47 @@
 package controller;
 
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 import model.Player;
 
+import network.ClientResponseGenerator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import program.Constants;
 import view.INetworkView;
 
 public class NewController {
-	
-	INetworkView view;
-	GameStateManager gameStateManager;
-	static Logger log = LogManager.getLogger(NewController.class.getName());
-	
-	public NewController(INetworkView view) {
-		this.view = view;
-		this.gameStateManager = new GameStateManager();
-	}
 
-	/*
-	 * Adds the player that is resembled by the running instance of this program.
-	 */
-	public void addLocalPlayer(int id) {
-		String name = view.getLocalPlayerName();
-		gameStateManager.addPlayer(id, name);
-	}
-	
-	/*
-	 * Adds the players that will act as opponents.
-	 */
-	public void addPlayer(int id, String name) {
-		gameStateManager.addPlayer(id, name);
-	}
+    INetworkView view;
+    GameStateManager gameStateManager;
+    static Logger log = LogManager.getLogger(NewController.class.getName());
+
+    public NewController(INetworkView view) {
+        this.view = view;
+        this.gameStateManager = new GameStateManager();
+    }
+
+    /*
+     * Adds the player that is resembled by the running instance of this program.
+     */
+    public void addLocalPlayer(int id) {
+        String name = view.getLocalPlayerName();
+        gameStateManager.addPlayer(id, name);
+    }
+
+    /*
+     * Adds the players that will act as opponents.
+     */
+    public void addPlayer(int id, String name) {
+        gameStateManager.addPlayer(id, name);
+    }
+
+    public void run() {
+        join();
+    }
+
+
+    private void join() {
+        ClientResponseGenerator.sendJoinGame(Constants.getSupportedVersions(),Constants.getSupportedFeatures());
+    }
 }
---
+++ src/program/Constants.java
@@ -0,0 +1,17 @@
+package program;
+
+
+public class Constants {
+    // TODO to be replaced with a configuration file
+
+    public static int[] getSupportedVersions() {
+        int[] versions = {1};
+        return versions;
+    }
+
+    public static String[] getSupportedFeatures() {
+        // returns an empty array until extra features are specified
+        String[] features = {};
+        return features;
+    }
+}
