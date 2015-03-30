0d695d3a6bb4dbc3fe577b278344f1cc6b6db596
mm280 <mm280@st-andrews.ac.uk>
2015-03-30 17:05:55 +0100

    Server handles multiple connections: UNTESTED

--- src/network/RiskServer.java
+++ src/network/RiskServer.java
@@ -1,7 +1,12 @@
 package network;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.minlog.Log;
+import controller.ServerController;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -11,15 +16,45 @@ import java.io.IOException;
 public class RiskServer {
 
     private Server server;
+    private JSONObject serverResponse = null;
+    ServerController controller;
 
     public RiskServer() throws IOException{
         server = new Server();
         registerPackets();
-        //todo controller?
-        //server.addListener(new ServersNetworkListener());
+        server.addListener(new Listener(){
+            public void connected(Connection connection) {
+                Log.info("[Server] Someone is trying to connect.");
+            }
+
+            public void received(Connection connection, Object object) {
+                if(object instanceof NetworkPacket){
+                    NetworkPacket clientMessage = (NetworkPacket)object;
+                    //TODO CALL CONTROLLER
+                    if(serverResponse!=null) {
+                        server.sendToAllExceptTCP(connection.getID(), serverResponse);
+                    }else{
+                        Log.info("[SERVER] Received null response object from controller");
+                    }
+                }
+            }
+            public void disconnected(Connection connection) {
+                Log.info("[Server] Someone is trying to disconnect.");
+            }
+                      });
         server.bind(54555);
         //TODO TIME OUT
         server.start();
+
+
+    }
+
+    /**
+     * Used by controller to send messages to the server to be sent to clients
+     * @param message a JSONObject with message to be sent
+     */
+    public void sendServerResponse(JSONObject message){
+        setServerResponse(message);
     }
 
     //Register all of packets that will be sent, packet will be something that
@@ -32,6 +67,9 @@ public class RiskServer {
         kryo.register(org.json.JSONArray.class);
         kryo.register(java.util.ArrayList.class);
 
+    }
+    public void setServerResponse(JSONObject serverResponse) {
+        this.serverResponse = serverResponse;
     }
 
 
--- src/network/ServersNetworkListener.java
+++ src/network/ServersNetworkListener.java
@@ -1,69 +1,14 @@
 package network;
 
-import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.minlog.Log;
-import controller.ServerController;
-import org.json.JSONObject;
-
-import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  * Created by ${mm280} on 18/02/15.
  */
 public class ServersNetworkListener extends Listener {
 
-    ServerController controller;
-    public ServersNetworkListener(ServerController controller) {
-        this.controller = controller;
-    }
 
-    //todo kick client from game??
-    ApiMethods api = new ServerApiManager(controller);
-    private Client client;
-    private Queue<Client> clients =  new LinkedList<>();
 
-    public void init(Client client){
-        this.client=client;
 
-    }
-    public void connected(Connection connection) {
-        Log.info("[Server] Someone is trying to connect.");
-        Thread t  = new Thread(client);
-        clients.add(client);
-        t.run();
-    }
 
-    public void disconnected(Connection connection) {
-        Log.info("[Server] Someone is trying to disconnect.");
-    }
-
-    /**
-     * Called when a server receives a request
-     * @param connection The client connection
-     * @param object The request from the client
-     */
-    public void received(Connection connection, Object object) {
-        if(object instanceof NetworkPacket){
-            NetworkPacket response = (NetworkPacket)object;
-            JSONObject message = api.parseResponse(response.getJsonStringResponse().toString());
-            api.checkCommandRequest(message);
-            // TODO the following line should be executed by the controller, not here
-            //connection.sendTCP(server_response);
-            // TODO call methods to look at response
-
-        }
-    }
-
-    /**
-     * Forward a message to all clients
-     * @param message message to be sent, in the form of a string
-     */
-    public void broadcast(String message) {
-        for(Client client : clients) {
-            client.sendTCP(message);
-        }
-    }
 }
