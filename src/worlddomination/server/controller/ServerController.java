29b96a9a2d915580081b01eb944ba88dcc6172d8
mm280 <mm280@st-andrews.ac.uk>
2015-04-20 04:09:28 +0100

    Changed continue button

--- src/worlddomination/server/controller/ServerController.java
+++ src/worlddomination/server/controller/ServerController.java
@@ -64,8 +64,7 @@ public class ServerController implements Runnable{
     		port = server.port;
     		view.addUpdate(portChange);
     	}
-        PingThread a = new PingThread();
-        a.run();
+
         while (true) {
             executeActions();
         }
@@ -186,7 +185,10 @@ public class ServerController implements Runnable{
             server.sendToOne(player_id,response);
             beginGame=true;
         }
-
+        if(connections.size()==3){
+            PingThread a = new PingThread();
+            a.run();
+        }
         if(beginGame || connections.size()==6) {
             response = responseGenerator.pingGenerator(playersJoined.size(), -1);
             server.sendMessageToAll(response);
