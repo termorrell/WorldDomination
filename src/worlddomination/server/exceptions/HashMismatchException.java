69711d688b8886d127d73fe5e664b53b57c6ebeb
cih4 <cih4@st-andrews.ac.uk>
2015-04-21 18:04:15 +0100

    protocol die roll

---
+++ src/worlddomination/server/exceptions/HashMismatchException.java
@@ -0,0 +1,9 @@
+package worlddomination.server.exceptions;
+
+public class HashMismatchException extends Exception {
+
+	public HashMismatchException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
